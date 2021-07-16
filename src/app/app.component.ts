c6a0b1b30e7daacd1d0a7a4883d27079f20bac70
samwel-chege <samwelchegeh09@gmail.com>
2021-07-16 21:14:21 +0300

    nest quotecomponent inside the appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  quotes:Quote[]= [
-    {id:1, name: 'sometimes my genius is almost frightening!'},
-    {id:2, name: 'It always seem impossible until it is done'},
-    {id:3, name: 'Every moment is a fresh beginning'}
-  ];
+  
 
   
   
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+
+  quotes:Quote[]= [
+    {id:1, name: 'sometimes my genius is almost frightening!', description: 'Quotes by Jeremy Clarkson'},
+    {id:2, name: 'It always seem impossible until it is done', description: 'Quotes by Mahatma Gandhi'},
+    {id:3, name: 'Every moment is a fresh beginning', description: 'Quotes by T.S Eliot'}
+  ]
 
   constructor() { }
 
