e7dcaf43fccd965e85622cb1bf3d5b819c7e2c5f
samwel-chege <samwelchegeh09@gmail.com>
2021-07-17 10:48:53 +0300

    add a new array

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,9 +8,12 @@ import { Quote } from '../quote';
 export class QuoteComponent implements OnInit {
 
   quotes:Quote[]= [
-    new Quote(1, 'sometimes my genius is almost frightening!', 'Quotes by Jeremy Clarkson'),
-    new Quote(2, 'It always seem impossible until it is done', 'Quotes by Mahatma Gandhi'),
-    new Quote(3, 'Every moment is a fresh beginning', 'Quotes by T.S Eliot')
+    new Quote(1, 'Sometimes my genius is almost frightening!', 'Quotes by Jeremy Clarkson'),
+    new Quote(2, 'It always seem impossible until it is done.', 'Quotes by Mahatma Gandhi'),
+    new Quote(3, 'Every moment is a fresh beginning.', 'Quotes by T.S Eliot'),
+    new Quote(4, 'Everything you can imagine is real.', 'Quote by Pablo Picasso'),
+    new Quote(5, 'Simplicity is the ultimate sophistication.', 'Quote by Leonardo da Vinci'),
+    new Quote(6, 'Whatever you do, do it well.', ' Quote by Walt Disney')
   ];
    toggleDetails(index:any){
      this.quotes[index].showDescription = !this.quotes[index].showDescription;
