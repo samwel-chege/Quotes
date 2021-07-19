1aa87f293d74f11aa979dd1eb4ad058c92e0d474
samwel-chege <samwelchegeh09@gmail.com>
2021-07-19 13:23:58 +0300

    add comment

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -9,7 +9,7 @@ export class DateCountPipe implements PipeTransform {
     let dateCount: string= '';
     let today: any= new Date();
     var difference = Math.abs(value - today) / 1000;
-    var days = Math.floor(difference/86400);
+    var days = Math.floor(difference/86400);//60secs*60mins *24hrs in a day
     if (days > 0){
       dateCount += Math.floor(days) + "days";
 
